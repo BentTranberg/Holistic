6a11d2d963ccbcadbd427cc34095bf6f4bc886ae
Bent Tranberg <bent.tranberg@gmail.com>
2022-07-12 20:43:07 +0200

    Holistic is Elmish.WPF
    Turned Holistic and Holistic.Models into an Elmish.WPF application.

---
+++ Holistic.Models/MainWindow.fs
@@ -0,0 +1,30 @@
+﻿module Holistic.Models.MainWindow
+
+open Elmish
+open Elmish.WPF
+
+type Model =
+    {
+        Reply: string
+    }
+
+type Msg =
+    | SayHello
+
+let startModel =
+    {
+        Reply = "Waiting for reply"
+    }
+
+let init () = startModel, Cmd.none
+
+let update (msg: Msg) (m: Model) : Model * Cmd<Msg> =
+    match msg with
+    | SayHello -> { m with Reply = "Hello, world!" }, Cmd.none
+
+let bindings () : Binding<Model,Msg> list = [
+    "SayHello" |> Binding.cmd SayHello
+    "Reply" |> Binding.oneWay (fun m -> m.Reply)
+    ]
+
+let designVm = ViewModel.designInstance startModel (bindings ())
