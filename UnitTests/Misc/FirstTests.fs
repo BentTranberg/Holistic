5e01889c699ac5e4ab94466701bbc2fd86df5e6a
Bent Tranberg <bent.tranberg@gmail.com>
2022-07-12 21:13:48 +0200

    Project UnitTests
    Added project UnitTests, using NUnit test framework.

---
+++ UnitTests/Misc/FirstTests.fs
@@ -0,0 +1,16 @@
+﻿module Misc.FirstTests
+
+open NUnit.Framework
+open Swensen.Unquote
+
+[<Test>]
+let ``The first test``() =
+    let expected = 7
+    let result = 3
+    test <@ result = expected @>
+
+[<Test>]
+let ``The second test``() =
+    let expected = 9
+    let result = 9
+    test <@ result = expected @>
