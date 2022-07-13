bc00b378e961fe15c3c60f54de500446db398e40
Bent Tranberg <bent.tranberg@gmail.com>
2022-07-13 08:27:40 +0200

    Some unit tests for SQLite functions

---
+++ Holistic.Business/TodoDb.fs
@@ -0,0 +1,50 @@
+﻿namespace Holistic.Business
+
+open Microsoft.Data.Sqlite
+open Dapper
+open Dapper.Contrib
+open Dapper.Contrib.Extensions
+
+module DbBasics =
+
+    let [<Literal>] connectionString = @"Data Source=C:\devbox\Holistic\Holistic.Business\bin\Debug\net6.0\todo.sqlite"
+
+    let createConn () = new SqliteConnection(connectionString)
+
+    /// Maps a nullable record (or possibly other types) to option.
+    let optionOfNullableRecord (record: 'T) : 'T option =
+        match record |> box with
+        | null -> None
+        | _ -> Some record
+
+    let querySingleOrDefault<'T> (conn: SqliteConnection, sql: string, args: obj) : 'T option =
+        conn.QuerySingleOrDefault<'T> (sql, args) |> optionOfNullableRecord
+
+    let getEntity<'T when 'T : not struct> (conn: SqliteConnection, id: obj) : 'T option =
+        conn.Get<'T> id |> optionOfNullableRecord
+
+[<Table "Todo"; CLIMutable>]
+type EntTodo =
+    {
+        Id: int64
+        Title: string
+        Description: string option
+    }
+
+module EntTodo =
+
+    let getAll () : EntTodo seq =
+        let conn = DbBasics.createConn ()
+        conn.GetAll<EntTodo> ()
+
+    let get (id: int) : EntTodo option =
+        let conn = DbBasics.createConn ()
+        DbBasics.getEntity<EntTodo> (conn, id)
+
+    let update (ent: EntTodo) : bool =
+        let conn = DbBasics.createConn ()
+        conn.Update<EntTodo> ent
+
+    let insert (ent: EntTodo) : int64 =
+        let conn = DbBasics.createConn ()
+        conn.Insert<EntTodo> ent
---
+++ UnitTests/Db/TodoTests.fs
@@ -0,0 +1,26 @@
+﻿module Db.TodoTests
+
+open NUnit.Framework
+open Swensen.Unquote
+open Holistic.Business
+
+[<Test>]
+let ``Call EntToDo dot get``() =
+    let result = EntTodo.get 1
+    let expected: EntTodo option =
+        Some { Id = 1; Title = "First"; Description = None }
+    test <@ result = expected @>
+
+    let result = EntTodo.get 2
+    let expected: EntTodo option =
+        Some { Id = 2; Title = "Second"; Description = Some "" }
+    test <@ result = expected @>
+
+    let result = EntTodo.get 3
+    let expected: EntTodo option =
+        Some { Id = 3; Title = "Third"; Description = Some "This is the third issue." }
+    test <@ result = expected @>
+
+    let result = EntTodo.get 4
+    let expected: EntTodo option = None
+    test <@ result = expected @>
